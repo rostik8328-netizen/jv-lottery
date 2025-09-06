2f17f06ca1f03dbe048d279bbb346b7119463e31
rostik8328-netizen <rostik8328@gmail.com>
2025-09-06 14:38:39 +0300

    create a simple lottery

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,9 +6,7 @@ public class ColorSupplier {
 
     public Color getRandomColor() {
 
-        Random random = new Random();
         int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color;
+        return Color.values()[index];
     }
 }
